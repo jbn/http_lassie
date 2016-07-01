83d2f382ab00fb1f010aef406c69a6c0459e182b
John Bjorn Nelson <jbn@abreka.com>
2016-07-01 11:25:30 -0400

    Add first draft

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name="http_lassie",
+    version="0.0.1",
+    packages=["http_lassie"],
+    license="License :: OSI Approved :: MIT License",
+    author="John Bjorn Nelson",
+    author_email="jbn@abreka.com",
+    description="Fetch Lassie.",
+    long_description="JS rendering proxy brokered requesting",
+    url="https://github.com/jbn/mimic"
+)
