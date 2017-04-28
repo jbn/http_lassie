2e81e8a3b0c37fe49167f25856962024f1d07df4
John B Nelson <jbn@abreka.com>
2017-04-28 10:41:10 -0400

    Add formatting to exceptions

--- http_lassie/smart_fetcher.py
+++ http_lassie/smart_fetcher.py
@@ -1,7 +1,8 @@
 import json
 import time
 import requests
-import traceback
+import sys
+from http_lassie.user_agents import random_user_agent
 from six.moves.urllib.parse import urlencode
 
 ###############################################################################
@@ -12,6 +13,28 @@ from six.moves.urllib.parse import urlencode
 #                 \____/\___/ \_/\_/   \____/\___| \_/ \___|_|                #
 #                                                                             #
 ###############################################################################
+
+
+class FailingStatusCode(Exception):
+    def __init__(self, status_code, resp_content, indent=''):
+        self.indent = indent
+        self.status_code = status_code
+        self.resp_content = resp_content
+
+    def __str__(self):
+        try:
+            if self.resp_content.startswith(b"{"):
+                json_body = json.loads(self.resp_content.decode())
+                err_doc = json.dumps(json_body, indent='    ')
+                lines = err_doc.splitlines()
+                return (lines[0] + "\n" +
+                        "\n".join(self.indent + line for line in lines[1:]))
+            else:
+                return ("status code: {}\n".format(self.status_code) +
+                        self.indent + "{}".format(self.resp_content))
+        except Exception as e:
+            return "ERROR PRINTING ERROR on {}".format(e)
+
 
 def get_proxy(mimic_server, request_url, requirements, max_wait_time=60):
     """
@@ -55,7 +78,7 @@ def release_proxy(mimic_server, proxy_resource, resp_time=-1,
         # Must be positive! XXX: BUG
         params['response_time'] = resp_time
     else:
-        print("RESP TIME".format(resp_time))
+        print("RESP TIME: {}".format(resp_time))
 
     resp = requests.post(mimic_server + "/proxies/release", data=params)
     assert resp.status_code == 200, resp.content
@@ -72,6 +95,23 @@ def release_proxy(mimic_server, proxy_resource, resp_time=-1,
 
 
 DEFAULT_SPLASH_CONFIG = {'timeout': 60, 'wait': 10, 'images': 0}
+
+EXCEPTION_MSG = """
+Request exception:
+    Request url:  {}
+    Retries left: {}
+    Type:         {}
+    Value:        {}
+    Location:     {}:{}
+""".strip()
+
+
+def format_exception(url, retries_left, exc_info):
+    exc_type, exc_value, exc_traceback = exc_info
+    line = exc_traceback.tb_lineno
+    file_name = exc_traceback.tb_frame.f_code.co_filename
+    print(EXCEPTION_MSG.format(url, retries_left,
+                               exc_type, exc_value, file_name, line))
 
 
 def diagnostic_extractor(resp):
@@ -121,7 +161,7 @@ class SmartFetcher:
                  http_method='GET',
                  extractor=content_extractor,
                  validator=always_true,
-                 render_json=False,
+                 render_js=False,
                  splash_overrides=None,
                  header_overrides=None,
                  retries=3):
@@ -135,7 +175,7 @@ class SmartFetcher:
             response object.
         :param validator: the function to call to validate the extracted
             content before returning
-        :param render_json: if True, use splash to render the JS
+        :param render_js: if True, use splash to render the JS
         :param splash_overrides: a optional dictionary of values to send
             with the splash request
         :param retries: maximum number of retries before failing
@@ -145,61 +185,20 @@ class SmartFetcher:
         content, resp_time, is_failure = None, 60, True
 
         while is_failure and retries > 0:
-            # Get the proxy to work with.
-            proxy_resource = get_proxy(self._mimic_server,
-                                       request_url,
-                                       self._proxy_requirements,
-                                       max_wait_time=self._max_wait_time)
-            if proxy_resource['proxy'] is None:
-                time.sleep(60)  # HACK: KLUDGE
-                retries -= 1
-                continue
+            proxy_resource, status_received = None, None
 
-            status_received = None
             try:
-                headers = {'User-Agent': random_user_agent()}
-                if header_overrides:
-                    headers.update(header_overrides)
+                proxy_resource = self._get_proxy_resource(request_url)
+                headers = self._common_headers(header_overrides)
 
-                if render_json:  # Use splash server
-                    kwargs = {**self._splash_config,
-                              **(splash_overrides or {})}
-                    kwargs.update({'proxy': proxy_resource['proxy'].lower(),
-                                   'timeout': self._max_wait_time,
-                                   'wait': 10,  # Render wait time
-                                   'http_method': http_method,
-                                   'filters': "nofonts,easylist"})
-
-                    # body but use encoded params
-                    if request_params:
-                        request_url += "?" + urlencode(request_params)
-                    elif request_data:
-                        kwargs['body'] = urlencode(request_params)
-
-                    kwargs['url'] = request_url
-                    kwargs['headers'] = headers
-
-                    start_time = time.time()
-                    resp = requests.post(self._splash_server + '/render.html',
-                                         data=json.dumps(kwargs),
-                                         headers={"Content-Type": "application/json"})
-                else:  # Just use requests
-                    kwargs = {'timeout': self._max_wait_time}
-
-                    if proxy_resource['proxy'].startswith('HTTPS'):
-                        kwargs['proxies'] = {'https': proxy_resource['proxy']}
-                    else:
-                        kwargs['proxies'] = {'http': proxy_resource['proxy']}
-
-                    if request_params:
-                        kwargs['params'] = request_data
-                    if request_data:
-                        kwargs['data'] = request_data
-
-                    kwargs['headers'] = headers
-
-                    start_time = time.time()
-                    resp = requests.request(http_method, request_url, **kwargs)
+                f = self._via_splash if render_js else self._via_requests
+                start_time, resp = f(headers,
+                                     http_method,
+                                     request_url,
+                                     request_params,
+                                     request_data,
+                                     proxy_resource,
+                                     **(splash_overrides or {}))
 
                 resp_time = time.time() - start_time
                 content = extractor(resp)
@@ -213,18 +212,86 @@ class SmartFetcher:
                                   False)
                     break
                 else:
-                    assert resp.status_code == 200, (resp.status_code, resp.content)
+                    if resp.status_code != 200:
+                        raise FailingStatusCode(resp.status_code,
+                                                resp.content,
+                                                indent=' '*18)
                     is_failure = not validator(content)
             except Exception as e:
                 is_failure = True
                 retries -= 1
+                print(format_exception(request_url,
+                                       retries,
+                                       sys.exc_info()))
                 # TODO: Add better logging
-                print("RETRYING {} on {} for {}".format(retries,
-                                                        request_url,
-                                                        status_received))
             finally:
                 # BUG BUG BUB
                 release_proxy(self._mimic_server, proxy_resource, resp_time,
                               is_failure)
 
         return content, not is_failure
+
+    def _get_proxy_resource(self, request_url):
+        resource = get_proxy(self._mimic_server,
+                             request_url,
+                             self._proxy_requirements,
+                             max_wait_time=self._max_wait_time)
+
+        if resource['proxy'] is None:
+            time.sleep(60)
+            raise Exception('No proxy found')
+
+        return resource
+
+    def _common_headers(self, header_overrides):
+        headers = {'User-Agent': random_user_agent()}
+        if header_overrides:
+            headers.update(header_overrides)
+        return headers
+
+    def _via_splash(self, headers, http_method, request_url, request_params,
+                    request_data, proxy_resource, **splash_overrides):
+        kwargs = {**self._splash_config,
+                  **(splash_overrides or {})}
+        kwargs.update({'proxy': proxy_resource['proxy'].lower(),
+                       'timeout': self._max_wait_time,
+                       'wait': 10,  # Render wait time
+                       'http_method': http_method,
+                       'filters': "nofonts,easylist"})
+
+        # body but use encoded params
+        if request_params:
+            request_url += "?" + urlencode(request_params)
+        elif request_data:
+            kwargs['body'] = urlencode(request_params)
+
+        kwargs['url'] = request_url
+        kwargs['headers'] = headers
+
+        start_time = time.time()
+        resp = requests.post(self._splash_server + '/render.html',
+                             data=json.dumps(kwargs),
+                             headers={"Content-Type": "application/json"})
+        return start_time, resp
+
+    def _via_requests(self, headers, http_method, request_url,
+                      request_params, request_data, proxy_resource,
+                      **kwargs):
+        kwargs = {'timeout': self._max_wait_time}
+
+        if proxy_resource['proxy'].startswith('HTTPS'):
+            kwargs['proxies'] = {'https': proxy_resource['proxy']}
+        else:
+            kwargs['proxies'] = {'http': proxy_resource['proxy']}
+
+        if request_params:
+            kwargs['params'] = request_data
+        if request_data:
+            kwargs['data'] = request_data
+
+        kwargs['headers'] = headers
+
+        start_time = time.time()
+        resp = requests.request(http_method, request_url, **kwargs)
+
+        return start_time, resp
